2c1f6b585d9dacede493d22910c626b2af740648
SHAHENSHAH31 <tyagichanchal407@gmail.com>
2025-06-04 14:28:59 +0530

    change

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,8 @@ const mongoose= require('mongoose');
 
 
 const connectDatabase =()=>{
-    mongoose.connect("mongodb://127.0.0.1:27017/MERN",{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
+    console.log(process.env.DB_URL);
+    mongoose.connect(process.env.DB_URL,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
 console.log(`mongodb connected with server: ${data.connection.host}`);
 }).catch((err)=>{
     console.log(err);
