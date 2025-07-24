8279ba6d7f72baaad7f3b49576348f0dceb7e23e
SHAHENSHAH31 <tyagichanchal407@gmail.com>
2025-07-24 13:48:57 +0530

    change

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const express = require("express");
 const cors=require('cors')
+const cors=require('cors')
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middleware/error");
 const bodyParser = require("body-parser");
